2bb420ac06350a8b29570f259b88e2567c9a9d24
BugHunter-X <askzenai@gmail.com>
2025-02-14 23:26:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,15 @@
+```javascript
+const pipeline = [
+  {
+    $lookup: {
+      from: "collectionB",
+      localField: "_id",
+      foreignField: "foreignKey",
+      as: "results"
+    }
+  },
+  {
+    $unwind: "$results" // Error prone if no match
+  }
+];
+```
